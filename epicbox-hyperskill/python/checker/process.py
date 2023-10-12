05d912ec35284d2214f72f1ca37c7661bfb28786
Sergey <polischuk0107@gmail.com>
2023-10-12 15:48:47 +0300

    fixed the feedback when the tests are failing

--- epicbox-hyperskill/python/checker/process.py
+++ epicbox-hyperskill/python/checker/process.py
@@ -9,24 +9,20 @@ if __name__ == '__main__':
     score = 1
     feedback = ''
     output = []
-    capture_feedback = False
     code = open('code.txt').read().strip()
     stdout = open('stdout.txt').read().splitlines()
     stderr = open('stderr.txt').read().splitlines()
     if code != OK_CODE:
         score = 0
-        for line in stderr:
+        for line_index, line in enumerate(stderr):
             if line.startswith(ASSERTION_ERROR_LINE):
-                capture_feedback = True
                 output.append(line[len(ASSERTION_ERROR_LINE):])
-                continue
-            if capture_feedback:
-                output.append(line)
+                output.extend(stderr[line_index + 1:])
+                break
         feedback = '\n'.join(output).strip()
     elif any(line.startswith(FAILED_TEST_BEGIN) for line in stdout):
         score = 0
         output_started = False
-
         for line in stdout:
             if output_started and line.startswith(FAILED_TEST_CONTINUE):
                 output.append(line[len(FAILED_TEST_CONTINUE):])
